21d8014a5dc4aaa40a1a1c12d84e04f24a5224af
Bang Chi Duong <chibangduong1993@gmail.com>
2018-07-21 15:52:07 -0700

    Change structure to deploy

--- server.js
+++ server.js
@@ -61,10 +61,10 @@ app.get('*', (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../frontend/build"));
   const path = require('path');
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
