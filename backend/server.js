853ea8fe30458ef88fcc30ef1c30bd6ad771d40d
Bang Chi Duong <chibangduong1993@gmail.com>
2018-07-21 15:05:30 -0700

    Add things to server.js and path dependency for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,14 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve('build', 'index.html'));
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, function() {
  console.log(`API running on port ${port}`);
 });
