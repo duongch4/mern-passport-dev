908e3aed6d86bf788ab48131b8998cb31a447783
Bang Chi Duong <chibangduong1993@gmail.com>
2018-07-23 16:51:02 -0700

    added the secrets on heroku

--- server.js
+++ server.js
@@ -62,10 +62,10 @@ app.get('*', (req, res) => {
 
 // if (process.env.NODE_ENV === "production") {
   // const path = require('path');
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-  });
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 // }
 
 app.listen(port, function() {
