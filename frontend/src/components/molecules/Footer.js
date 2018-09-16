636ecda97c8332ad291bb4f7c6fdd75e687991c5
Bang Chi Duong <chibangduong1993@gmail.com>
2018-09-16 19:33:05 -0400

    Change Footer

--- frontend/src/components/molecules/Footer.js
+++ frontend/src/components/molecules/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
   return (
     <nav className="footer navbar-dark bg-dark static-bottom text-center">
       <div className="social-icons">
-        <a href="https://www.linkedin.com/in/bang-chi-duong-6857969a/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" /></a>
-        <a href="https://github.com/duongch4" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons" /></a>
+        <a href="https://www.linkedin.com/in/bang-chi-duong/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin social-icons" /></a>
+        <a href="https://github.com/duongch4/mern-passport-dev/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons" /></a>
         <a href="mailto:bangchi.duong.20193@outlook.com?Subject=Hello%20there!" target="_top"><i className="fas fa-envelope-square social-icons" /></a>
       </div>
       
       <div className="navbar-text">
-        CPSC 490 - MERN Project - {new Date().getFullYear()}
+        MERN Project - {new Date().getFullYear()}
       </div>
     </nav>
   );
